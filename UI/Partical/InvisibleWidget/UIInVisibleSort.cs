272881e42acec8066dc71b59192e67629fdbfc35
xiaoyaoxiansheng <360507245@qq.com>
2018-12-27 14:50:03 +0800

    add ui_partical_invisibleWidget

---
+++ UI/Partical/InvisibleWidget/UIInVisibleSort.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(UIInvisibleWidget))]
+public class UIInVisibleSort : MonoBehaviour
+{
+    public bool isTop = true;
+    public int renderQueue_ReadOnly;
+
+    private Renderer[] _renderers;
+    private UIInvisibleWidget _widget;
+    private bool _isSort = false;
+
+    public void SortTop()
+    {
+        int maxDepth = _widget.depth;
+        if (isTop)
+        {
+            UIPanel parentPanel = transform.GetComponentInParent<UIPanel>();
+            if (parentPanel)
+                maxDepth = parentPanel.GetMaxUIWightDepth() + 1;
+        }
+        Sort(maxDepth);
+    }
+
+    public void Sort(int depth)
+    {
+        _widget.depth = depth;
+        _isSort = true;
+    }
+
+    void Awake()
+    {
+        _widget = gameObject.GetComponent<UIInvisibleWidget>();
+        _renderers = gameObject.GetComponentsInChildren<Renderer>();
+    }
+
+    void Start()
+    {
+        SortTop();
+    }
+
+    void Update()
+    {
+        if (!_isSort)
+            return;
+
+        if (_widget == null || _widget.drawCall == null || _renderers == null)
+        {
+            return;
+        }
+
+        _isSort = false;
+
+        renderQueue_ReadOnly = _widget.drawCall.renderQueue;
+        foreach(Renderer render in _renderers)
+        {
+            //render.sortingOrder = 0;
+            render.material.renderQueue = renderQueue_ReadOnly;
+        }
+    }
+
+}
---
+++ UI/Partical/InvisibleWidget/UIInvisibleWidget.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIInvisibleWidget : UIWidget
+{
+    private Texture2D mTex;
+    private Material mMat;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        height = 0;
+        width = 0;
+        mTex = new Texture2D(0, 0);
+        mChanged = false;
+        fillGeometry = false;
+
+        geometry.verts.Add(new Vector3(1f, 0, 0));
+        geometry.verts.Add(new Vector3(2f, 0, 0));
+        geometry.verts.Add(new Vector3(3f, 0, 0));
+        geometry.verts.Add(new Vector3(4f, 0, 0));
+        geometry.ApplyTransform(Matrix4x4.identity);
+        geometry.uvs.Add(Vector2.zero);
+        geometry.uvs.Add(Vector2.up);
+        geometry.uvs.Add(Vector2.one);
+        geometry.uvs.Add(Vector2.right);
+        geometry.cols.Add(Color.white);
+        geometry.cols.Add(Color.white);
+        geometry.cols.Add(Color.white);
+        geometry.cols.Add(Color.white);
+    }
+
+    public override Texture mainTexture
+    {
+        get
+        {
+            return mTex;
+        }
+        set
+        {
+
+        }
+    }
+
+    public override Material material
+    {
+        get
+        {
+            return mMat;
+        }
+        set
+        {
+
+        }
+    }
+}
